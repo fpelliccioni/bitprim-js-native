c69bf35659a0d5617d4ec3484258a4366fb475b6
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-01-17 17:13:44 -0300

    conan db full flag

--- conanfile.py
+++ conanfile.py
@@ -6,20 +6,20 @@ class BitprimJs(ConanFile):
     version = "0.1"
     license = "http://www.boost.org/users/license.html"
     url = "https://github.com/bitprim/bitprim-js"
-    description = "Bitcoin Full Node Library with Javascript interface"
+    description = "Javascript API for the Bitcoin Full Node"
     settings = "os", "compiler", "build_type", "arch"
     
     # options = {"shared": [True, False]}
     # default_options = "shared=False"
 
     generators = "cmake"
-    # exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-node-cintConfig.cmake.in", "include/*", "test/*", "console/*"
-    # package_files = "build/lbitprim-node-cint.so"
-    # build_policy = "missing"
 
     # TODO(fernando): queda pendiente seleccionar el default Shared=False
     
+
+    # TODO(fernando): Set macros for the several DB modes
     requires = (("bitprim-node-cint/0.18.0@bitprim/stable"))
+    default_options = {"bitprim-node-cint:db": "full"}
 
     def imports(self):
         self.copy("*.h", "./deps/include/bitprim", "include/bitprim")
@@ -30,7 +30,3 @@ class BitprimJs(ConanFile):
         self.copy("*.so", dst="./deps/lib", src="lib")
         self.copy("*.dll", dst="./deps/lib", src="lib")
 
-
-    # def build(self):
-    # def package(self):
-    # def package_info(self):
