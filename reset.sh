c3fc895e8cfb7603de811d48484c85b763f14b54
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-01-18 11:47:43 -0300

    reset script

---
+++ reset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+rm -rf build/
+rm -rf deps/
+rm -rf lib/
+rm -rf node_modules/
+rm -rf package-lock.json
+rm -rf conan_imports_manifest.txt
+rm -rf conanbuildinfo.cmake
+rm -rf conanbuildinfo.txt
+rm -rf conaninfo.txt
